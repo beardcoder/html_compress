f0007afff963eae0f9dfcee82c1b1a7d71bfd5ac
Markus Sommer <markus.sommer@in2code.de>
2016-12-24 22:45:38 +0100

    [FEATURE] ext_emconf and generator

---
+++ Build/GenerateExtEmConf.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+
+$composerJson = json_decode(file_get_contents('../composer.json'), true);
+$version = getenv('EXT_VERSION');
+if (empty($version)) {
+    throw new \Exception('No version set use EXT_VERSION="1.0.0-dev ./GenerateExtEmConf.php', 1482615696);
+}
+
+$extEmConfContent = [
+    'title' => $composerJson['name'],
+    'description' => $composerJson['description'],
+    'category' => 'FE',
+    'author' => $composerJson['authors'][0]['name'],
+    'author_email' => $composerJson['authors'][0]['email'],
+    'author_company' => $composerJson['authors'][0]['homepage'],
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'version' => $version,
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.5.0-7.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    $composerJson['autoload']
+];
+
+$extEmConfContent = '<?php
+
+$EM_CONF[$_EXTKEY] = ' . var_export($extEmConfContent, true) . ';
+';
+
+file_put_contents('../ext_emconf.php', $extEmConfContent);
---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'markussom/html-compress',
+  'description' => 'Compress/minify your HTML',
+  'category' => 'FE',
+  'author' => 'Markus Sommer',
+  'author_email' => 'info@creativeworkspace.de',
+  'author_company' => 'https://www.creativeworkspace.de/',
+  'state' => 'stable',
+  'clearCacheOnLoad' => 1,
+  'lockType' => '',
+  'version' => '1.0.0-dev',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.5.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  0 => 
+  array (
+    'psr-4' => 
+    array (
+      'Markussom\\HtmlCompress\\' => 'Classes',
+    ),
+  ),
+);
